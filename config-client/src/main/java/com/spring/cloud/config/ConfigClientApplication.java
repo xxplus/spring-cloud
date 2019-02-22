d9eab0efe31cfe57098677e2a6c884a270e3c946
over <abc.bady@gmail.com>
2019-02-22 22:54:25 +0800

    Cofig Client获取配置文件值启动失败，原因不明。

--- config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
@@ -1,26 +1,15 @@
 package com.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableConfigServer
-@SpringBootConfiguration
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-//    @Value("${foo}")
-//    private String foo;
-//
-//    @RequestMapping(value = "/hi")
-//    public String hi(){
-//        return foo;
-//    }
 }
---
+++ config-client/src/main/java/com/spring/cloud/config/controller/MessageController.java
@@ -0,0 +1,18 @@
+package com.spring.cloud.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
+}
