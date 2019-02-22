d3130a1b84ea010dc7f07428051b90e75230d41e
LiFang <lifang@winployee.com>
2019-02-22 15:28:58 +0800

    学习Spring Cloud Config配置，DEMO提交。

---
+++ config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+//    @Value("${foo}")
+//    private String foo;
+//
+//    @RequestMapping(value = "/hi")
+//    public String hi(){
+//        return foo;
+//    }
+}
