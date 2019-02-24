09010605865f6f9f1ce5a858e00b6e131b5c09c7
over <abc.bady@gmail.com>
2019-02-24 22:23:58 +0800

    集群Cofig Client获取配置文件值启动失败，原因不明，提交

--- config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
@@ -1,15 +1,24 @@
 package com.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(false);
+        return c;
     }
 
 }
