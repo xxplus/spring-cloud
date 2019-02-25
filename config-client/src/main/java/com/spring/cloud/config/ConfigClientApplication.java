676dbe19518718419f9a3071f38cef0ca4744bd2
LiFang <lifang@winployee.com>
2019-02-25 16:38:27 +0800

    学习Spring Cloud Config Bus DEMO提交

--- config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
@@ -1,11 +1,21 @@
 package com.spring.cloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -18,5 +28,14 @@ public class ConfigClientApplication {
         c.setIgnoreUnresolvablePlaceholders(false);
         return c;
     }
+
+    @Value("${profile}")
+    private String profile;
+
+    @RequestMapping(value = "/profile")
+    public String profile(){
+        return profile;
+    }
+
 
 }
