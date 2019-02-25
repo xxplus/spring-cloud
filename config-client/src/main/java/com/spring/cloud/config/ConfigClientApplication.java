934dfc3986fd059fa913bf8a77cf28aa66df637b
LiFang <lifang@winployee.com>
2019-02-25 15:04:01 +0800

    学习Spring Cloud Config配置，DEMO提交, Config Client启动不了问题修复，不能使用github改用oschina#gitee就好了。

--- config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/config/ConfigClientApplication.java
@@ -1,11 +1,9 @@
 package com.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
